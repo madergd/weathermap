6399e0e90c4a4450fdd91f5631dae7fb844859d1
madergd <madergd@yahoo.co.uk>
2016-07-10 15:07:18 +0100

    Create weather.js

---
+++ src/main/web/javascript/weather.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+    $("input:radio[name='city']").change(function(event) {
+        getWeather();
+    });
+    $("input:radio[name='temperature']").change(function(event) {
+        getWeather();
+    });
+});
+
+function getWeather(){
+    var cityname = $("input:radio[name='city']:checked").val();
+    var temp = $("input:radio[name='temperature']:checked").val();
+    if(cityname && temp) {
+        $.get('ActionServlet', {temperature: temp, city: cityname}, function (responseText) {
+            $('#selectedcity').text(responseText.name);
+            $('#date').text(responseText.date);
+            $('#sunrise').text("Sunrise: " + responseText.sunrise + " a.m.");
+            $('#sunset').text("Sunset: " + responseText.sunset + " p.m.");
+            $('#temp').text("Temperature: " + responseText.temp + " ");
+            $('#temp').append("&deg;" + responseText.symbol);
+            $('#description').text(responseText.description);
+        });
+    }
+}
